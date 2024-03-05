7653dbbe75f54f6766a95f381d902786d9ba5148
tusharravenclaw <tushar68053@gmail.com>
2024-03-05 22:29:09 +0530

    basic bootstrapped pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
