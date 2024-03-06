f5b04bf3f51bf6ccc50aa9474cf5c2484ebdaabe
tusharravenclaw <tushar68053@gmail.com>
2024-03-07 00:26:53 +0530

    basic frontend

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,26 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  const stars = [];
+
+  for (let i = 1; i <= 5; i++) {
+    if (value > i) {
+      stars.push(<FaStar key={i} />);
+    } else if (value >= i - 0.5) {
+      stars.push(<FaStarHalfAlt key={i} />);
+    } else {
+      stars.push(<FaRegStar key={i} />);
+    }
+  }
+
+  return (
+    <div className="rating">
+      {stars.map((star, index) => (
+        <span key={index}>{star}</span>
+      ))}
+      <span className="rating-text">{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
